97b8b695df0779688ce57b988e06bbb307a7ceb9
vinh <phamvanvinh8a2@gmail.com>
2024-03-20 22:56:18 +0700

    setup

---
+++ src/main/java/org/vinhpham/sticket/STicketApplication.java
@@ -0,0 +1,13 @@
+package org.vinhpham.sticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class STicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(STicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/vinhpham/sticket/STicketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vinhpham.sticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class STicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
